f59c47287bd699f238c122372969d3b99d0c3bea
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2012-05-01 17:22:05 +0300

    lesson 1 step4  - more printing exercises

--- lesson1/step4_hi2.py
+++ lesson1/step4_hi2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# look down for %r, %d, %s usage
 print "more formatting"
 student="cow"
 lesson = 1
@@ -7,3 +8,11 @@ print "Hi %s, this is lesson %d and step%d" %(
     student,lesson,step)
 print "\nI said\n"
 print "Hi {0}, this is lesson {1} and step{2}".format(student,lesson,step)
+
+x="hell this is lesson %d" % 1
+print x
+print "oh no! %r" % x
+
+x = "No tidings Elven-folk have heard"
+y = "Of Amroth evermore."
+print x + y
