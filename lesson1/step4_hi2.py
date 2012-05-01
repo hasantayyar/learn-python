cf108d09fb7bc561d2c97e296ac8b9020fed725e
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2012-05-01 17:13:23 +0300

    lesson1 step4

---
+++ lesson1/step4_hi2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+print "more formatting"
+student="cow"
+lesson = 1
+step = 4
+print "Hi %s, this is lesson %d and step%d" %(
+    student,lesson,step)
+print "\nI said\n"
+print "Hi {0}, this is lesson {1} and step{2}".format(student,lesson,step)
