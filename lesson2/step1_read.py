577fe99a6fc621661ef1dbb4e3957374e8bbd5d6
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2012-05-06 01:11:26 +0300

    lesson 2 - file examples

--- lesson2/step1_read.py
+++ lesson2/step1_read.py
@@ -3,3 +3,18 @@ txt = open(filename)
 data = txt.read()
 print "content %d bytes long " % len(data)
 print data
+
+#########
+
+txt = open(filename)
+oneline = txt.readline()
+print oneline
+oneline = txt.readline()
+print oneline
+oneline = txt.readline()
+print oneline
+
+txt.seek(0)
+oneline = txt.readline()
+print oneline
+
